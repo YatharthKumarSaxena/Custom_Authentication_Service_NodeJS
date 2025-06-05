2654e07dfd4e51e8cbec302857d4f6074bfbccd3
YatharthKumarSaxena <yatharthsaxena25@gmail.com>
2025-06-05 11:37:38 +0530

    Defined User Schema in Model using required,unique,default,minLength,type etc. features to introduce valid constraints

---
+++ Models/User.model.js
@@ -0,0 +1,89 @@
+const mongoose = require("mongoose");
+
+/* User Schema */
+
+/*
+ * Name
+ * User_ID
+ * Password
+ * Email_ID
+ * User Type
+ * Address [localAddress,city,pincode,state,country]
+ * Phone Number
+ * isVerified
+ * isActive
+ * UserType
+ * lastLogin
+ * verificationToken
+*/
+
+const userSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    phoneNumber:{
+        type: String,
+        match: /^[0-9]{10}$/,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 8 // strong password practice
+    },
+    userID:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    emailID:{
+        type: String,
+        unique: true,
+        required: true
+    },
+    address:[
+        {
+            localAddress:{
+                type: String,
+                required: true
+            },
+            city:{
+                type: String,
+                required: true
+            },
+            pincode:{
+                type: String,
+                required: true
+            },
+            state:{
+                type: String,
+                required: true
+            },
+            country:{
+                type: String,
+                required: true
+            }
+        }
+    ],
+    isActive:{
+        type: Boolean,
+        default: true
+    },
+    isVerified:{
+        type: Boolean,
+        default: false
+    },
+    userType:{
+        type: String,
+        enum: ["Customer","Admin"],
+        default: "Customer"
+    },
+    lastLogin:{
+        type: Date,
+    },
+    verificationToken: {
+        type: String // for OTP / email verification link
+    }
+},{timestamps:true,versionKey:false})
