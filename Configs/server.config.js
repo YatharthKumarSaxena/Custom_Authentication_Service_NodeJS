e3e29fbe9ab784c78d4ab22eddd738f5bba5c5a1
YatharthKumarSaxena <yatharthsaxena25@gmail.com>
2025-06-05 16:23:13 +0530

    Made Database config file in Configs Folder, used mongoose to connect with MongoDB Successfully

--- Configs/server.config.js
+++ Configs/server.config.js
@@ -1,3 +1,4 @@
+/* Since Port Number can Change , hence it is a Configurable Value and placed in Configs Folder */
 module.exports = {
     PORT_NUMBER: 8080
 }
--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 // This is the File from where the whole Project Will Start Running
 
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require("express"); // Extract Express Module
+const mongoose = require("mongoose"); // Extract Mongoose Module
 const serverConfigs = require("./Configs/server.config");
-const app = express();
+const app = express(); // App is an Express Function
+const dbConfigs = require("./Configs/db.config")
 
+/*
+ * Create an Admin User if not Exits at the Start of the Application
+ */
+
+// Connection with MongoDB
+mongoose.connect(dbConfigs.DB_URL); // Specifying where to connect
+
+const db = mongoose.connection; // Ordering to Connect
+
+db.on("err",()=>{
+    console.log("Error Occured while Connecting to Database");
+    console.log("Error Occured is displayed below:- ");
+    console.log(err);
+})
+
+db.once("open",()=>{
+    console.log("Connection estabalished with MongoDB Succesfully");
+})
+
+// Initializing Server by Express
 app.listen(serverConfigs.PORT_NUMBER,()=>{
-    console.log("Server has Started at Port Number: "+serverConfigs.PORT_NUMBER);
+    // Check Server is Running or not
+    console.log("Server has Started at Port Number: "+serverConfigs.PORT_NUMBER); 
 });
