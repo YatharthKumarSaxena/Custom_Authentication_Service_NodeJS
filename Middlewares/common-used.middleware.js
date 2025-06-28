ec7d1724521c4893cea6edb3de15f66abfb6f388
YatharthKumarSaxena <yatharthsaxena25@gmail.com>
2025-06-28 16:16:57 +0530

    Now verify device field middleware is successfully hadling malformed requests like invalid device uuid format and device uuid not provided requests

--- Middlewares/common-used.middleware.js
+++ Middlewares/common-used.middleware.js
@@ -298,26 +298,26 @@ const verifyDeviceField = async (req,res,next) => {
         const deviceType = req.headers["x-device-type"]; // Optional
         // Device ID is mandatory
         if (!deviceID || deviceID.trim() === "") {
-            logMiddlewareError("Verify Device Field, Device ID field Missing");
+            logWithTime("Verify Device Field, Device ID field Missing");
             return throwResourceNotFoundError(res, "Device UUID (x-device-uuid) is required in request headers");
         }
         // Attach to request object for later use in controller
         req.deviceID = deviceID.trim();
         if (!isValidRegex(deviceID,UUID_V4_REGEX)) {
-            logMiddlewareError("Verify Device Field, Invalid Device ID format");
+            logMiddlewareError("Verify Device Field, Invalid Device ID format",req);
             return throwInvalidResourceError(res, `Device UUID, Provided Device UUID (${req.deviceID}) is not in a valid UUID v4 format`);
         }
         if (deviceName && deviceName.trim() !== "") {
             deviceName = deviceName.trim();
             if(!validateLength(deviceName,deviceNameLength.min,deviceNameLength.max)){
-                logMiddlewareError(`Verify Device Field, Invalid Device Name length.`);
+                logMiddlewareError(`Verify Device Field, Invalid Device Name length.`,req);
                 return throwAccessDeniedError(res, `Device Name length should be between ${deviceNameLength.min} and ${deviceNameLength.max} characters`);
             }
         }
         if (deviceType && deviceType.trim() !=="") {
             const type = deviceType.toUpperCase().trim();
             if (!DEVICE_TYPES.includes(type)) {
-                logMiddlewareError("Verify Device Field, Invalid Device Type Provided");
+                logMiddlewareError("Verify Device Field, Invalid Device Type Provided",req);
                 return throwInvalidResourceError(res, `Device Type. Use Valid Device Type: ${DEVICE_TYPES}`);
             }
             req.deviceType = type;
