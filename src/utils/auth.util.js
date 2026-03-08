991bd2d84446864fc02ba59e1b21ae295303b9d5
YatharthKumarSaxena <yatharthsaxena25@gmail.com>
2026-03-08 20:38:05 +0530

    feat: Add field-definition and user-type-checker utilities, enhance auth and ID validation utils

--- src/utils/auth.util.js
+++ src/utils/auth.util.js
@@ -1,9 +1,8 @@
 const { UserModel } = require("@models/user.model");
 const bcryptjs = require("bcryptjs");
 const { isValidRegex, validateLength } = require("./validators-factory.util");
-const { phoneNumberRegex, userIdRegex } = require("../configs/regex.config");
+const { phoneNumberRegex } = require("../configs/regex.config");
 const { phoneNumberLength } = require("../configs/fields-length.config");
-const { adminIdPrefix } = require("@/configs/id-prefixes.config");
 const { SALT } = require("@/configs/security.config");
 
 const checkPasswordIsValid = async (userId, providedPassword) => {
@@ -20,11 +19,6 @@ const hashPassword = async (plainPassword) => {
     return await bcryptjs.hash(plainPassword, SALT);
 };
 
-const isAdminId = (userId) =>
-    typeof userId === "string" &&
-    isValidRegex(userId, userIdRegex) &&
-    userId.startsWith(adminIdPrefix);
-
 const createPhoneNumber = (countryCode, number) => {
     const newNumber = "+" + countryCode + number;
     if (!validateLength(newNumber, phoneNumberLength.min, phoneNumberLength.max)) {
@@ -39,6 +33,5 @@ const createPhoneNumber = (countryCode, number) => {
 module.exports = {
     createPhoneNumber,
     checkPasswordIsValid,
-    hashPassword,
-    isAdminId
+    hashPassword
 }
